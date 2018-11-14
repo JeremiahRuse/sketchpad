b0ffed877e4e901239ad212a856c2e4fbcebe721
Jeremiah Ruse <rusejeremiah@gmail.com>
2018-11-14 12:40:48 -0500

    Create grid

--- sketchpad.js
+++ sketchpad.js
@@ -1,5 +1,31 @@
+const container = document.getElementById("container");
+
+createGrid(15);
+
 function createGrid(gridSize) {
-    
+    console.log(gridSize);
+    let boxSize = 500 / gridSize;
+    for (horizontalIndex = 0; horizontalIndex < gridSize; ++horizontalIndex) {
+        console.log(horizontalIndex);
+        let horizontalDiv = document.createElement("div");
+        horizontalDiv.style.setProperty('width', '500px');
+        horizontalDiv.style.setProperty('height', boxSize + 'px');
+        horizontalDiv.classList.add("horizontal");
+        container.appendChild(horizontalDiv);
+
+        for (verticalIndex = 0; verticalIndex < gridSize; ++verticalIndex) {
+
+            let verticalDiv = document.createElement("div");
+            verticalDiv.style.setProperty('width', boxSize + 'px');
+            verticalDiv.style.setProperty('height', boxSize + 'px');
+            verticalDiv.style.setProperty('background-color', 'blue');
+            verticalDiv.style.setProperty('display', 'inline-block');
+            verticalDiv.classList.add("vertical");
+            horizontalDiv.appendChild(verticalDiv);
+            console.log(horizontalIndex.toString + " " + verticalIndex.toString);
+
+        }
+    }
 }
 
 function changeColor(objectToChange) {
